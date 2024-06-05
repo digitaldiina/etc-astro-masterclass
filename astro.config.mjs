002a186009963344e8a8778209b0962ac78ff1a0
Bergfrid Skaara Dias <bergfridd@superoffice.com>
2024-06-05 11:01:51 +0200

    initial translation setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+        defaultLocale: "en",
+        locales: ["de", "en", "no"],
+        routing: {
+            prefixDefaultLocale: false
+        },
+        fallback: {
+            no: "en"
+        }
+    }
+});
